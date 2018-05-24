0a722e65943f93144a0073b7784e9c2e0c832ee3
Francis <francisjhackenberger@gmail.com>
2018-05-24 19:18:50 -0400

    Added theme starter assets including css, javascript, and images

--- themes/splice/assets/javascript/app.js
+++ themes/splice/assets/javascript/app.js
@@ -1,48 +1,130 @@
-/*
- * Application
- */
+(function($, document, window){
 
-$(document).tooltip({
-    selector: "[data-toggle=tooltip]"
-})
+	$(document).ready(function(){
 
-/*
- * Auto hide navbar
- */
-jQuery(document).ready(function($){
-    var $header = $('.navbar-autohide'),
-        scrolling = false,
-        previousTop = 0,
-        currentTop = 0,
-        scrollDelta = 10,
-        scrollOffset = 150
+		$("[data-background]").each(function(){
+			var retina = window.devicePixelRatio > 1;
+			var bg = $(this).data("background");
+			if( retina ){
+				var retinabg = bg.replace(".jpg","@2x.jpg");
+				$(this).css("background-image","url("+retinabg+")");	
+			} else{
+				$(this).css("background-image","url("+bg+")");
+			}
+			
+		});
 
-    $(window).on('scroll', function(){
-        if (!scrolling) {
-            scrolling = true
+		$("[data-bg-color]").each(function(){
+			var bg = $(this).data("bg-color");
+			$(this).css("background-color",bg);
+		});
 
-            if (!window.requestAnimationFrame) {
-                setTimeout(autoHideHeader, 250)
-            }
-            else {
-                requestAnimationFrame(autoHideHeader)
-            }
-        }
-    })
+		$(".slider").flexslider({
+			directionNav: false,
+			controlNav: true,
+		});
 
-    function autoHideHeader() {
-        var currentTop = $(window).scrollTop()
+		$(".quote-slider").flexslider({
+			directionNav: true,
+			controlNav: false,
+			prevText: "<i class='fa fa-caret-left'></i>",
+			nextText: "<i class='fa fa-caret-right'></i>",
+		});
 
-        // Scrolling up
-        if (previousTop - currentTop > scrollDelta) {
-            $header.removeClass('is-hidden')
-        }
-        else if (currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
-            // Scrolling down
-            $header.addClass('is-hidden')
-        }
+		var eventCarousel = $(".event-carousel");
+		eventCarousel.owlCarousel({
+ 
+			autoPlay: 3000, //Set AutoPlay to 3 seconds
+			rewindNav: false,
+			items : 4,
+			itemsDesktop : [1199,3],
+			itemsDesktopSmall : [979,3]
 
-        previousTop = currentTop
-        scrolling = false
-    }
-});
+		});
+		// Custom Navigation Events
+		$("#event-next").click(function(e){
+			e.preventDefault();
+			eventCarousel.trigger('owl.next');
+		});
+		$("#event-prev").click(function(e){
+			e.preventDefault();
+			eventCarousel.trigger('owl.prev');
+		});
+
+		var $container = $('.filterable-items');
+
+		$container.imagesLoaded(function(){
+		    $container.isotope({
+		        filter: '*',
+		        layoutMode: 'fitRows',
+		        animationOptions: {
+		            duration: 750,
+		            easing: 'linear',
+		            queue: false
+		        }
+		    });
+		});
+		$('.filterable-nav a').click(function(e){
+	    	e.preventDefault();
+	        $('.filterable-nav .current').removeClass('current');
+	        $(this).addClass('current');
+
+	        var selector = $(this).attr('data-filter');
+	        $container.isotope({
+	            filter: selector,
+	            animationOptions: {
+	                duration: 750,
+	                easing: 'linear',
+	                queue: false
+	            }
+	         });
+	         return false;
+	    });
+	    $('.mobile-filter').change(function(){
+
+	        var selector = $(this).val();
+	        $container.isotope({
+	            filter: selector,
+	            animationOptions: {
+	                duration: 750,
+	                easing: 'linear',
+	                queue: false
+	            }
+	         });
+	         return false;
+	    });
+
+	    initLightbox({
+	    	selector : '.filterable-item a',
+	    	overlay: true,
+	    	closeButton: true,
+	    	arrow: true
+	    });
+
+	    $(".mobile-menu").append($(".main-navigation .menu").clone());
+	    $(".toggle-menu").click(function(){
+	    	$(".mobile-menu").slideToggle();
+	    });
+
+	    if( $(".map").length ){
+			$('.map').gmap3({
+				map: {
+					options: {
+						maxZoom: 14,
+						scrollwheel: false
+					}  
+				},
+				marker:{
+					address: "40 Sibley St, Detroit",
+				}
+			},
+			"autofit" );
+	    }
+
+	});
+
+	$(window).ready(function(){
+
+	});
+
+})(jQuery, document, window);
